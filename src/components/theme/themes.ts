6784d2dc179d046d3949ee7679c6ef62e0c54b14
Karl J. Overå <k@rl.run>
2018-11-07 21:38:20 +0100

    More theme tweak

--- src/components/theme/themes.ts
+++ src/components/theme/themes.ts
@@ -12,26 +12,56 @@ const base = {
   'negative-color-contrast': 'white',
 }
 
-const light = {
-  ...base,
-}
+const light = {}
 
 const dark = {
-  ...base,
   'global-background': 'darkslategray',
   'font-color': 'white',
   'contrast-background': 'white',
   'contrast-font-color': 'darkslategray',
 }
 
-export const availableThemes = ['dark', 'light']
+const black = {
+  'global-background': 'black',
+  'font-color': 'gray',
+  'contrast-background': '#1b1b1b',
+  'contrast-font-color': 'darkslategray',
+  'accent-color': 'darkslategray',
+  'accent-contrast-color': 'lightgray',
+  'positive-color': 'darkslategray',
+  'negative-color': 'darkred',
+  'positive-color-contrast': 'black',
+  'negative-color-contrast': 'black',
+}
 
-export default (theme: 'light' | 'dark' | null) => {
+const validateTheme = (theme: { [key: string]: string }) => {
+  const baseKeys: string[] = Object.keys(base)
+  const themeKeys: string[] = Object.keys(theme)
+
+  const duplicates: string[] = []
+
+  baseKeys.forEach(value => {
+    if (themeKeys.indexOf(value) === -1) duplicates.push(value)
+  })
+
+  return duplicates.join('\n')
+}
+
+console.info('Theme completeness:')
+console.info('light:\n', validateTheme(light))
+console.info('dark:\n', validateTheme(dark))
+console.info('black:\n', validateTheme(black))
+
+export const availableThemes = ['dark', 'light', 'black']
+
+export default (theme: string | null) => {
   switch (theme) {
     case 'dark':
-      return dark
+      return { ...base, ...dark }
     case 'light':
-      return light
+      return { ...base, ...light }
+    case 'black':
+      return { ...base, ...black }
     default:
       throw Error('Oops: Invalid theme')
   }
